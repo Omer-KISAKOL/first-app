fb4ea5c3444f11a4bc00d6acf83682a0849c450f
ÖMER KISAKOL <okisakol@gmail.com>
2023-11-19 07:17:17 +0300

    Add to Cart

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 'use client';
 import React from 'react'
+import AddtoCart from './AddtoCart';
 
 const ProductCard = () => {
   return (
     <div>
-        <button onClick ={() => console.log('Click')} >Add to Cart</button>
+        <AddtoCart/>
     </div>
   )
 }
